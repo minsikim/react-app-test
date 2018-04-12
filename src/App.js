3065191f064429b57f46b4abb217278505ca3794
Salinger MS <minsiua@gmail.com>
2018-04-13 08:14:49 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p1>Hello it's a React App test</p1>
+      </div>
+    );
+  }
+}
+
+export default App;
