635224690b24b6c6bf35735fc8db8e66312a5d4e
Salinger MS <minsiua@gmail.com>
2018-04-13 22:24:59 +0900

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -13,31 +13,79 @@ class App extends Component {
       { name: 'Granma', age: 62}
     ]
   }
-switchNameHandler = () => {
+switchNameHandler = (newName) => {
   console.log('switchNameHandler run')
   //don't : this.state.persons[0].name = 'MinSeongKim'
   // -> this will not work in ES6 => syntax
   //because this refers to the function you are wrinting code in
+  newName = newName ? newName : 'SalingerMS'
+
   this.setState({
     persons: [
-      { name: 'SalingerMS', age: 29},
+      { name: newName, age: 29},
       { name: 'MS', age: 31},
       { name: 'Salinger', age: 21},
       { name: 'Granma', age: 99}
-    ]
+    ],
+    showPersons: false
   })
 }
 
+nameChangeHandler = (event) => {
+  console.log('nameChangeHandler run')
+  this.setState(
+    {
+      persons: [
+        { name: 'Minsi', age: 29},
+        { name: event.target.value, age: 31},
+        { name: 'Salinger', age: 21},
+        { name: 'Granma', age: 99}
+      ]
+    }
+  )
+}
+
+togglePersonsHandler = () => {
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow})
+}
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
     return (
       <div className="App">
         <h1>Hello it's a React App test</h1>
         <p>This is working</p>
-        <button onClick={this.switchNameHandler}>Switch</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Hello is MinSeong</Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <button 
+          style = {style}
+          onClick={()=>this.togglePersonsHandler()}>Show</button>
+        { this.state.showPersons ? 
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}/>
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              change={this.nameChangeHandler}/>
+            <Person
+            //if you need to pass on params on the click method you can
+            // 1. bind(this, params) or
+            // 2. ()=>function()
+            // #1 is better
+              click = {this.switchNameHandler.bind(this, 'something')}
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}>Hello is MinSeong</Person>
+            <Person
+              name={this.state.persons[3].name}
+              age={this.state.persons[3].age}/>
+          </div> : null
+        }
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, "Hello it's a React App test"));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name}, {props.age} years old!</p>
-            {props.children}
+        <div className='Person'>
+            <p onClick={props.click}>I'm {props.name}, {props.age} years old!</p>
+            <p> {props.children} </p>
+            <input onChange={props.change} value={props.name} />
         </div>
     )
 }
