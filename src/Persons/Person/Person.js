7589fb986a7dad6905a73b2c41eb662d234f992f
Salinger MS <minsiua@gmail.com>
2018-08-20 03:22:10 +0900

    using proptypes package

--- src/Persons/Person/Person.js
+++ src/Persons/Person/Person.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
+import PropTypes from 'prop-types'
 
-const person = (props) => (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name}, {props.age} years old!</p>
-            <p> {props.children} </p>
-            <input onChange={props.change} value={props.name} />
+class Person extends Component{
+    render(){
+        return <div className={classes.Person}>
+            <p onClick={this.props.click}>I'm {this.props.name}, {this.props.age} years old!</p>
+            <input onChange={this.props.change} value={this.props.name} />
         </div>
-    )
+    }
+};
+//타입스크립트 같은 역할 하면서 받는 자료형을 에러로 띄워줌 되면 되는데 에러로 알려줌
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
-export default person;
+export default Person;
